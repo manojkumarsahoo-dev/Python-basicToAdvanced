9f3a94fe4e6fb9de493b6c2bb1babf19ac0156de
Manoj Kumar Sahoo <kumarsahoomanoj078@gmail.com>
2026-09-18 20:36:09 +0530

    Delete Chapter 2 -ps directory

--- Chapter 2 -ps/02_problem2.py
+++
@@ -1,3 +0,0 @@
-a=34
-b=5
-print("Remainder is when a is divided by b is ",a%b)
--- Chapter 2 -ps/04_problem4.py
+++
@@ -1,4 +0,0 @@
-a = int (input("Enter the number 1 : "))
-b = int (input("Enter the number 2 : "))
-
-print("a is greater than b : ",a>b)
