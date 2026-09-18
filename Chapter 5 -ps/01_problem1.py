4ef796529efd4b314993754bce7c69fc47d3dcf1
Manoj Kumar Sahoo <kumarsahoomanoj078@gmail.com>
2026-09-18 20:39:30 +0530

    Delete Chapter 5 -ps directory

--- Chapter 5 -ps/01_problem1.py
+++
@@ -1,7 +0,0 @@
-words = {
-    "madad": "help",
-    "kursi": "chair",
-    "billi": "cat"
-}
-word = input("Enter a word in Hindi: ")
-print(words[word])
--- Chapter 5 -ps/09_problem9.py
+++
@@ -1,3 +0,0 @@
-s = {8,7,12,"Harry", [1,2]} #cant have list as set element
-
-s[4][0] = 9 # cant change list element as list is unhashable
