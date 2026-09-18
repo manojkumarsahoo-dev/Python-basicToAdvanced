52c4d11e63e843edeb1812cde4dd0cba48f94149
Manoj Kumar Sahoo <kumarsahoomanoj078@gmail.com>
2026-09-18 20:38:36 +0530

    Delete Chapter 7 -ps directory

--- Chapter 7 -ps/05_problem5.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter the number: ")) #sum of first n numbers 
-i=1
-sum=0
-while(i<=n):
-    sum += i
-    i+=1
-
-print(sum)
--- Chapter 7 -ps/06_problem6.py
+++
@@ -1,7 +0,0 @@
-#finding factorial 
-n = int(input("Enter the number: "))
-product =1
-for i in range(1, n+1): # we need 1 to n
-    product = product * i
-
-print(f"The factorial of {n} is {product}")    
