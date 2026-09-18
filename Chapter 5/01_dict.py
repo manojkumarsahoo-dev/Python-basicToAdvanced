1421b9a155c49d2adc6bcfb22b018874649d2f85
Manoj Kumar Sahoo <kumarsahoomanoj078@gmail.com>
2026-09-18 20:39:17 +0530

    Delete Chapter 5 directory

--- Chapter 5/01_dict.py
+++
@@ -1,7 +0,0 @@
-marks = {
-    "Harry": 100,
-    "Subham": 56,
-    "Rohan": 23
-}
-print(marks,type(marks))
-print(marks["Harry"])
--- Chapter 5/05_set_union_intersection.py
+++
@@ -1,5 +0,0 @@
-s1 = {1,45,6,78}
-s2 = {7,8,78,1}
-
-print(s1.union(s2))
-print(s1.intersection(s2))
