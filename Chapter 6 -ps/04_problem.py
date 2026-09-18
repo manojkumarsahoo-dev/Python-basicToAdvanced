6ac0f9c1a35f36ea4586ef92a6088d26f1520d5d
Manoj Kumar Sahoo <kumarsahoomanoj078@gmail.com>
2026-09-18 20:39:00 +0530

    Delete Chapter 6 -ps directory

--- Chapter 6 -ps/04_problem.py
+++
@@ -1,6 +0,0 @@
-username = input("Enter username: ")
-
-if(len(username)<10):
-    print("Your username contains less than 10 characters")
-else :
-    print("All is well")    
--- Chapter 6 -ps/07_problem7.py
+++
@@ -1,6 +0,0 @@
-post = input("Enter the post: ")
-
-if("harry" in post.lower()):
-    print("This post is talking about Harry")
-else:    
-    print("This post is not talking about Harry")
